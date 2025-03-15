f8a61d8db60176efe001357d44e39aa6047d1e77
jurabek10 <samiev.jurabek@bk.ru>
2025-03-16 03:42:26 +0900

    Completed

---
+++ frontend/src/store/product.js
@@ -0,0 +1,81 @@
+import { create } from "zustand";
+
+const useProductStore = create((set) => ({
+  products: [],
+
+  setProducts: (products) => set({ products }), // ✅ Now `set` is available
+
+  createProduct: async (newProduct) => {
+    if (!newProduct.name || !newProduct.price || !newProduct.image) {
+      return { success: false, message: "Please fill in all fields." };
+    }
+
+    try {
+      const res = await fetch("/api/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newProduct),
+      });
+
+      if (!res.ok) {
+        return { success: false, message: "Failed to create product." };
+      }
+
+      const data = await res.json();
+
+      set((state) => ({ products: [...state.products, data.data] })); // ✅ Now `set` is properly used
+
+      return { success: true, message: "Product Created successfully." };
+    } catch (error) {
+      console.error("Error creating product:", error);
+      return {
+        success: false,
+        message: "An error occurred. Please try again.",
+      };
+    }
+  },
+
+  fetchProducts: async () => {
+    const res = await fetch("/api/products");
+    const data = await res.json();
+    set({ products: data.data });
+  },
+
+  deleteProduct: async (pid) => {
+    const res = await fetch(`/api/products/${pid}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (!data.success) return { success: false, message: data.message };
+
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== pid),
+    }));
+    return { success: true, message: data.message };
+  },
+
+  updateProduct: async (pid, updatedProduct) => {
+    const res = await fetch(`/api/products/${pid}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedProduct),
+    });
+    const data = await res.json();
+    if (!data.success) return { success: false, message: data.message };
+
+    // update the ui immediately, without needing a refresh
+    set((state) => ({
+      products: state.products.map((product) =>
+        product._id === pid ? data.data : product
+      ),
+    }));
+
+    return { success: true, message: data.message };
+  },
+}));
+
+export default useProductStore;
